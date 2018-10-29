a029492804e8d9dafaffc62ce79cba24d99e1ca7
Zach Izzard <izzard@usc.edu>
2018-10-28 19:14:38 -0700

    Added browse page

---
+++ v1/frontend/browse/scripts.js
@@ -0,0 +1,41 @@
+function clickedHeart(element){
+	var elementID = element.getAttribute("data-itemID");
+	console.log(elementID);
+
+	if(element.innerHTML === "<i class=\"fas fa-heart\"></i>"){
+		element.innerHTML = "<i class='far fa-heart'></i>";
+		element.style.color = "white";
+
+	}else{
+		element.innerHTML = "<i class='fas fa-heart'></i>";
+		element.style.color = "#FF4755";
+	}
+};
+
+function getItem(element){
+	var elementID = element.getAttribute("data-itemID");
+	console.log(elementID);
+};
+
+function unsaveItem(element){
+	var elementID = element.getAttribute("data-itemID");
+	console.log(elementID);
+};
+
+function showSavedItems(){
+    var table = document.getElementById("saved-items");
+    if (table.style.display === "block") {
+         $('#saved-items').animate({width: "0%"}, 500);
+         setTimeout(function(){
+        	table.style.display = "none";
+    	},450);
+    } else {
+    	table.style.display = "block";
+        $('#saved-items').animate({width: "30%"}, 500);
+    }
+};
+
+$(window).scroll(function() {
+	var windowpos = $(window).scrollTop();
+    $('#saved-items').css('top', windowpos + "px");
+});
