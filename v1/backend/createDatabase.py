938ca272c360d578425b8ae49443c982db396590
Arthur Krut <akrut@usc.edu>
2018-11-08 21:25:02 -0800

    Updated script, need to fix clear items

--- v1/backend/createDatabase.py
+++ v1/backend/createDatabase.py
@@ -44,6 +44,9 @@ for location in locations:
 	meetup_location.insert_one(location)
 print("CREATED LOCATION COLLECTION")
 
+words = "Nulla facilisi. Nunc ut leo neque. Duis at purus at arcu feugiat elementum et eu magna. Aliquam imperdiet sem metus, eget sagittis metus pharetra sit amet. Maecenas in luctus mauris, et pretium orci. Maecenas tempor iaculis augue gravida porta. Nulla facilisi. Vivamus eros quam, venenatis quis ipsum eget, efficitur cursus tellus. Maecenas lacinia purus id pellentesque suscipit."
+words = words.split('.')
+
 itemInserted1 = item.insert_one({'title' : 'Bruin Bear', 'description' : 'Worst mascot ever', 
 	'condition' : 'Poor', 'forSale' : True, 'userSellingItem' : ObjectId(), 
 	'highestBidder' : ObjectId(), 'endForSaleDate' : 12342153, 'startForSaleDate' : 12342100, 'maxBid' : 0,
@@ -56,9 +59,24 @@ itemInserted2 = item.insert_one({'title' : 'Stanford Cardinal', 'description' : 
 	'image' : 'http://web.stanford.edu/~siegelr/stanford/homecoming2014/IMG_8867%20trees%2010-25-2014%20stanford%20stadium.JPG',
 	'location' : 'Los Angeles, CA', 'usersWatching' : []
 	})
+
+for i in range(0, 10):
+	itemToInsert = item.insert_one({
+			'title' : words[i],
+			'description' : 'This is my test description',
+			'condition' : 'Poor',
+			'forSale' : True,
+			'userSellingItem' : ObjectId(),
+			'highestBidder' : ObjectId(),
+			'endForSaleDate' : 12342153,
+			'startForSaleDate' : 12342153,
+			'maxBid' : 50,
+			'image' : "https://picsum.photos/300/?image=" + str(i*i*i),
+			'location' : 'Los Angeles, CA',
+			'usersWatching' : []
+		})
+
 print("CREATED ITEM COLLECTION")
-
-
 
 user.insert_one({'firstName' : 'Tommy', 'lastName' : 'Trojan', 
 	'phoneNumber' : '213-123-4567', 'zipCode' : 90089,
