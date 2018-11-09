ca9f9f48f138a8e118e6850b2d0e6edc86c0a1a3
Arthur Krut <akrut@usc.edu>
2018-11-08 23:20:04 -0800

    Finished search on backend, need to implement on frontend

--- v1/backend/createDatabase.py
+++ v1/backend/createDatabase.py
@@ -44,25 +44,11 @@ for location in locations:
 	meetup_location.insert_one(location)
 print("CREATED LOCATION COLLECTION")
 
-words = "Nulla facilisi. Nunc ut leo neque. Duis at purus at arcu feugiat elementum et eu magna. Aliquam imperdiet sem metus, eget sagittis metus pharetra sit amet. Maecenas in luctus mauris, et pretium orci. Maecenas tempor iaculis augue gravida porta. Nulla facilisi. Vivamus eros quam, venenatis quis ipsum eget, efficitur cursus tellus. Maecenas lacinia purus id pellentesque suscipit."
-words = words.split('.')
+college_mascots = ['Joe Bruin', 'Oski the Bear', 'Stanford Cardinal', 'Fighting Irish', 'OSU Buckeyes']
 
-itemInserted1 = item.insert_one({'title' : 'Bruin Bear', 'description' : 'Worst mascot ever', 
-	'condition' : 'Poor', 'forSale' : True, 'userSellingItem' : ObjectId(), 
-	'highestBidder' : ObjectId(), 'endForSaleDate' : 12342153, 'startForSaleDate' : 12342100, 'maxBid' : 0,
-	'image' : 'https://pbs.twimg.com/profile_images/952872960767025152/t_ssj_Uy_400x400.jpg',
-	'location' : 'Los Angeles, CA', 'usersWatching' : []
-	})
-itemInserted2 = item.insert_one({'title' : 'Stanford Cardinal', 'description' : 'Inferior football team',
-	'condition' : 'Poor', 'forSale' : True, 'userSellingItem' : ObjectId(),
-	'highestBidder' : ObjectId(), 'endForSaleDate' : 12342153, 'startForSaleDate' : 12342100, 'maxBid' : 0,
-	'image' : 'http://web.stanford.edu/~siegelr/stanford/homecoming2014/IMG_8867%20trees%2010-25-2014%20stanford%20stadium.JPG',
-	'location' : 'Los Angeles, CA', 'usersWatching' : []
-	})
-
-for i in range(0, 10):
+for i in range(0, 5):
 	itemToInsert = item.insert_one({
-			'title' : words[i],
+			'title' : college_mascots[i],
 			'description' : 'This is my test description',
 			'condition' : 'Poor',
 			'forSale' : True,
@@ -73,16 +59,12 @@ for i in range(0, 10):
 			'maxBid' : 50,
 			'image' : "https://picsum.photos/300/?image=" + str(i*i*i),
 			'location' : 'Los Angeles, CA',
+			'keywords' : ['college', 'mascot', 'tommy', 'trojan', 'USC'],
 			'usersWatching' : []
 		})
 
 print("CREATED ITEM COLLECTION")
 
-user.insert_one({'firstName' : 'Tommy', 'lastName' : 'Trojan', 
-	'phoneNumber' : '213-123-4567', 'zipCode' : 90089,
-	'googleUserId' : '1234567', 'userRating' : 5, 
-	'savedItems' : [{'itemId' : itemInserted1.inserted_id}, {'itemId' : itemInserted2.inserted_id}]
-	})
 print("CREATED USER COLLECTION")
 
 message.insert_one({'content' : 'This is a test message. Fight On!',
