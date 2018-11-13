e6a1c06ce22891a54945e9a351df170adf10e8e9
Arthur Krut <akrut@usc.edu>
2018-11-12 21:51:07 -0800

    Done with email

--- perigo/src/main/java/com/MongoController/perigo/controller/EmailController.java
+++ perigo/src/main/java/com/MongoController/perigo/controller/EmailController.java
@@ -15,21 +15,27 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.MongoController.perigo.models.MessageSent;
 
 @RestController
 public class EmailController {
 
-	@RequestMapping(value = "/sendemail")
-	public String sendEmailToUser() throws AddressException, MessagingException, IOException {
-		sendmail();
+	@RequestMapping(value="/sendmessage", method=RequestMethod.POST)
+	public String sendEmailToUser(@Valid @RequestBody MessageSent msg_sent) throws AddressException, MessagingException, IOException {
+		sendmail(msg_sent);
+		
 		return "Email sent successfully";
 	}   
 	
 	
-	private void sendmail() throws AddressException, MessagingException, IOException {
+	private void sendmail(MessageSent msg_sent) throws AddressException, MessagingException, IOException {
 		   Properties props = new Properties();
 		   props.put("mail.smtp.auth", "true");
 		   props.put("mail.smtp.starttls.enable", "true");
@@ -44,18 +50,19 @@ public class EmailController {
 		   Message msg = new MimeMessage(session);
 		   msg.setFrom(new InternetAddress("perigo.donotreply@gmail.com", false));
 
-		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("izzard@usc.edu"));
-		   msg.setSubject("Forgot Password");
-		   msg.setContent("Forgot Password", "text/html");
+		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(msg_sent.getReceiver()));
+		   System.out.println(msg_sent.getReceiver());
+		   msg.setSubject("Item Message");
+		   msg.setContent("Item Message", "text/html");
 		   msg.setSentDate(new Date());
 
 		   MimeBodyPart messageBodyPart = new MimeBodyPart();
 		   
-		   String content = "Hello,<br>"
-		   		+ "It looks like you forgot your password. If you did not request to change your password"
-		   		+ ", please disregard this email. Otherwise, please follow the instructions in the attached link.<br>"
-		  		+ "Thanks!";
-		   
+		   String content = "Hello " + msg_sent.getSellerName() +  ", <br>"
+				   + "You received a message from " + msg_sent.getSender() + ". <br>"
+				   + "Subject: " + msg_sent.getSubject() + "<br>"
+				   + "Content: " + msg_sent.getContent() + "<br>";
+		   System.out.println(content);
 		   
 		   messageBodyPart.setContent(content, "text/html");
 
